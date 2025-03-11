2d9e17451a88f26a879fb27b08d71042fd00f888
jihwan <jihwan1535@nate.com>
2025-03-11 20:10:13 +0900

    refactor(#34): Request 상세 검증 제거
    * 비즈니스 로직으로 전환할 예정

--- api/src/main/java/co/kr/cocomu/study/dto/request/JoinPrivateStudyDto.java
+++ api/src/main/java/co/kr/cocomu/study/dto/request/JoinPrivateStudyDto.java
@@ -1,11 +1,9 @@
 package co.kr.cocomu.study.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import org.hibernate.validator.constraints.Length;
 
 @Schema(description = "비공개 스터디 참여 요청")
 public record JoinPrivateStudyDto(
-    @Length(min = 4, max = 6)
     @Schema(description = "스터디 비밀번호", example = "1234")
     String password
 ) {
--- api/src/main/java/co/kr/cocomu/user/dto/request/UserJoinRequest.java
+++ api/src/main/java/co/kr/cocomu/user/dto/request/UserJoinRequest.java
@@ -1,12 +1,10 @@
 package co.kr.cocomu.user.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotBlank;
 
 @Schema(description = "사용자 추가 요청")
 public record UserJoinRequest(
     @Schema(description = "사용자 닉네임", example = "코코무")
-    @NotBlank
     String nickname
 ) {
 }
